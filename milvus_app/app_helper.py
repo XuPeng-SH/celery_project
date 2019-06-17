d91d1e06e239cd91450152a160876b7bbf506ae5
Xu Peng <xupeng3112@163.com>
2019-06-17 15:32:14 +0800

    create app supprt check testing config

--- milvus_app/app_helper.py
+++ milvus_app/app_helper.py
@@ -2,11 +2,12 @@ from celery import Celery
 from milvus_app import serializer
 from milvus_app import settings
 
-def create_app(testing_config=None, db=None):
+def create_app(db=None, testing=False):
     app = Celery(__name__,
             broker=settings.CELERY_BROKER_URL,
             backend=settings.CELERY_BACKEND_URL)
-    if testing_config:
+    if settings.TESTING:
+        testing_config = 'milvus_app.settings.TestingConfig'
         app.config_from_object(testing_config)
         db and db.init_db(uri=testing_config.DB_URI)
     else:
--- milvus_app/settings.py
+++ milvus_app/settings.py
@@ -6,13 +6,15 @@ from environs import Env
 env = Env()
 env.read_env()
 
+TESTING = env.bool('TESTING', False)
+
 DB_URI = env.str('DB_URI')
 
 CELERY_APP_CONFIG_FILE = env.str('CELERY_APP_CONFIG_FILE')
 CELERY_BROKER_URL = env.str('CELERY_BROKER_URL')
 CELERY_BACKEND_URL = env.str('CELERY_BACKEND_URL')
 
-# class TestingConfig:
-#     CELERY_APP_CONFIG_FILE = env.str('CELERY_APP_TEST_CONFIG_FILE', 'config_testing.py')
-#     TEST_DATA_SET_DIR = env.str(TEST_DATA_SET_DIR, './static_data/testing')
-#     TOPK_DATA_SET_FILE = 'topk_data_set_2k.json'
+class TestingConfig:
+    CELERY_APP_CONFIG_FILE = env.str('CELERY_APP_TEST_CONFIG_FILE', 'config_testing.py')
+    TEST_DATA_SET_DIR = env.str('TEST_DATA_SET_DIR', './static_data/testing')
+    TOPK_DATA_SET_FILE = 'topk_data_set_2k.json'
