619903d6d75b0842b478b9c6002a43e45eee2693
Xu Peng <xupeng3112@163.com>
2019-06-18 11:00:30 +0800

    add redis handler

---
+++ milvus_app/redis_handler.py
@@ -0,0 +1,23 @@
+import logging
+import redis
+
+logger = logging.getLogger(__name__)
+
+class RedisHandler:
+    def __init__(self, host=None, port=None, password=None, db=None):
+        self.host = host if host else 'localhost'
+        self.port = port if port else 6379
+        self.db = db if db else 0
+        self.password = password
+        self.client = None
+
+    def init(self, host=None, port=None, password=None, db=None):
+        self.host = host if host else self.host
+        self.port = port if port else self.port
+        self.db = db if db is not None else self.db
+        self.password = password if password else self.password
+
+        self.client = redis.Redis(host=self.host,
+                port=self.port,
+                password=self.password if self.password else None,
+                db=self.db)
