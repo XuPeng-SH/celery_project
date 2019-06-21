9a3c4e2fecac714a2bfeb5793650e813609f1042
Xu Peng <xupeng3112@163.com>
2019-06-21 16:53:24 +0800

    update config

--- apps/configurations/config.py
+++ apps/configurations/config.py
@@ -1,7 +1,7 @@
 CELERY_ACCEPT_CONTENT = ['customjson']
 CELERY_TASK_SERIALIZER = 'customjson'
 CELERY_RESULT_SERIALIZER = 'customjson'
-CELERY_CREATE_MISSING_QUEUES = False
+CELERY_CREATE_MISSING_QUEUES = True
 
 CELERY_ROUTES={
     'tasks.query_files' : {'queue': 'for_query'},
