c3309713f147a393d4d29a652728a317b46af817
Xu Peng <xupeng3112@163.com>
2019-06-26 21:06:07 +0800

    update db config

--- milvus_celery/db_base.py
+++ milvus_celery/db_base.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 class DB:
     Model = declarative_base()
@@ -8,11 +8,14 @@ class DB:
         uri and self.init_db(uri)
 
     def init_db(self, uri):
-        self.engine = create_engine(uri)
+        self.engine = create_engine(uri, pool_size=100, pool_recycle=5, pool_timeout=30,
+                pool_pre_ping=True,
+                max_overflow=0)
         self.uri = uri
         session = sessionmaker()
         session.configure(bind=self.engine)
         self.db_session = session()
+
 
     @property
     def Session(self):
