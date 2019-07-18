05e6fdcf649478a6c4a4f02bd6e1614a1f091bc5
peng.xu <peng.xu@zilliz.com>
2019-07-18 15:47:52 +0800

    change server model back to forking

--- apps/proxy_server/ThriftServer.py
+++ apps/proxy_server/ThriftServer.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse
 
 from thrift.protocol import TBinaryProtocol, TCompactProtocol, TJSONProtocol
 from thrift.transport import TTransport, TSocket, TZlibTransport
-from thrift.server import TServer
+from thrift.server.TServer import TForkingServer
 from thrift.server.TProcessPoolServer import TProcessPoolServer
 from thrift.server.TNonblockingServer import TNonblockingServer
 from milvus.thrift import MilvusService
@@ -38,7 +38,7 @@ class ThriftServer:
                     'Invalid configuration for THRIFTSERVER_TRANSPORT: {}'.format(
                         settings.THRIFTSERVER_TRANSPORT)
                     )
-    
+
     def set_factory(self):
         if settings.THRIFTSERVER_BUFFERED:
             self._tfactory = TTransport.TBufferedTransportFactory()
@@ -46,7 +46,7 @@ class ThriftServer:
             self._tfactory = TZlibTransport.TZlibTransportFactory()
         if settings.THRIFTSERVER_FRAMED:
             self._tfactory = TTransport.TFramedTransportFactory()
-            
+
         if settings.THRIFTSERVER_PROTOCOL == Protocol.BINARY:
             self._pfactory = TBinaryProtocol.TBinaryProtocolFactory()
         elif settings.THRIFTSERVER_PROTOCOL == Protocol.COMPACT:
@@ -62,7 +62,9 @@ class ThriftServer:
     def start_thrift_server(self):
         self.set_transport()
         self.set_factory()
-        self.server = TNonblockingServer(self.processor, self.transport)
+        self.server = TForkingServer(self.processor, self.transport, self._tfactory,
+                self._pfactory)
+        # self.server = TNonblockingServer(self.processor, self.transport)
 
         self.server.serve()
 
